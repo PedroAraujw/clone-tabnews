28b51c55ee9d6e4729509bb8f77b9b9d8846695b
Pedro Araujo <141824854+PedroAraujw@users.noreply.github.com>
2024-08-01 13:36:41 +0000

    adicina scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 um clone do site https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home (){
-    return <h1>Veritas filia temporis! p-p :)</h1>
+function Home() {
+  return <h1>Veritas filia temporis! p-p :)</h1>;
 }
 
 export default Home;
